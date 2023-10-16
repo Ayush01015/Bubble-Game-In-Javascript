c2cd40f5c957b09d9b206c661ac2a61c055b8f94
Ayush Shrivastav <ayushshrivastav575@gmail.com>
2023-10-16 18:04:31 +0530

    Creating bubble and timer dynamic

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Working...");
+
+
+
+function makeBubble(){
+    let clutter = "";
+    
+    for(let i = 1; i<=152; i++){
+        clutter += `<div class="bubble">${Math.floor(Math.random()*10)}</div>`
+    }
+    document.querySelector(".pBottom").innerHTML = clutter;
+}
+
+var timer = 60;
+
+function runTimer(){
+    setInterval(()=>{
+        document.querySelector(".timer").innerHTML = timer;
+        console.log(timer);
+        timer--;
+    },1000)
+}
+ 
+
+makeBubble();
+runTimer();
